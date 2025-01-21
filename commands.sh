491c0d113bd713fd4adf8a31cffefe0b40e3e992
Laurent ARTUSIO <laurent.artusio@orange.com>
2025-01-21 09:32:21 +0100

    Improve readme, delete obsolete file

--- commands.sh
+++ commands.sh
@@ -1,11 +1,9 @@
-# Build the required images
+# Manually muild the required images
 docker build -t -f elastic8.dockerfile . -t elastic8
 docker build -t -f kibana8.dockerfile . -t kibana8
 
-
 # Only if you want to test stuff apart from docker-compose. This is not required otherwise.
 docker network create --subnet=192.168.10.0/24 elastic
-
 
 #
 docker run --name elastic8 --net elastic -p 9200:9200 -it -m 1GB elastic8
